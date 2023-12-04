8e1c4850c77ea72019d5668f73d1a1f131ed18cc
Jakub Plecháč <plechac.k@gmail.com>
2023-12-04 20:00:19 +0100

    real_name to id

--- utils/slackBot.js
+++ utils/slackBot.js
@@ -4,7 +4,7 @@ const web = new WebClient(process.env.SLACK_API_KEY);
 
 export const createPrivateChannel = async (user) => {
   // Create a private channel with the user's username
-  const channelName = user.real_name; // Assuming username is unique
+  const channelName = user.id; // Assuming username is unique
   const createChannelResponse = await web.conversations.create({
     name: channelName,
     is_private: true,
