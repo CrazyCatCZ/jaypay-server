3262ea3c6d11d5834d4e524065f5f615cb70255a
Jakub Plecháč <plechac.k@gmail.com>
2023-12-04 20:06:57 +0100

    validate channel name

--- utils/slackBot.js
+++ utils/slackBot.js
@@ -2,9 +2,17 @@ import { WebClient } from "@slack/web-api";
 
 const web = new WebClient(process.env.SLACK_API_KEY);
 
+const validChannelName = (name) => {
+  // Replace invalid characters with underscores
+  const sanitized = name.replace(/[^a-z0-9-_]/g, "_");
+
+  // Ensure the length is 21 characters or less
+  return sanitized.substring(0, 21);
+};
+
 export const createPrivateChannel = async (user) => {
   // Create a private channel with the user's username
-  const channelName = user.id; // Assuming username is unique
+  const channelName = validChannelName(user.name);
   const createChannelResponse = await web.conversations.create({
     name: channelName,
     is_private: true,
